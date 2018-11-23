e128ac3fdfddbf07eaecc1129b23ec672867ca83
Christian Weichel <christian.weichel@typefox.io>
2018-11-23 08:31:26 +0000

    Configured travis to build and upload releases

---
+++ build/build_release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION="_"
+if [ ! -z "$TRAVIS_TAG" ]; then
+    VERSION="_${TRAVIS_TAG}_"
+fi
+
+go get -v github.com/mitchellh/gox
+go get -v github.com/inconshreveable/mousetrap
+go get -v github.com/konsorten/go-windows-terminal-sequences
+mkdir build/release
+gox -os="linux darwin windows" -arch="386 amd64" -output="build/release/ruruku$VERSION{{.OS}}_{{.Arch}}" -ldflags "-X main.Rev=`git rev-parse --short HEAD`" -verbose ./...
