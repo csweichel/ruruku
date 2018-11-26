2091f08af824e8a51cb735422e390720dfcccffe
Christian Weichel <christian.weichel@typefox.io>
2018-11-26 12:48:33 +0000

    Fixed #28

--- pkg/rurukulib/cli/init.go
+++ pkg/rurukulib/cli/init.go
@@ -12,25 +12,25 @@ type Init struct {
 }
 
 func (cfg *Init) checkOrAskString(value string, label string, dflt string, required bool, validation promptui.ValidateFunc) (string, error) {
-	if value == "" {
-		if required && cfg.NonInteractive {
-			return "", fmt.Errorf("Runnig non-interactive and there's no %s set", strings.ToLower(label))
-		}
+	if required && value == "" {
+        if cfg.NonInteractive {
+            return "", fmt.Errorf("Runnig non-interactive and there's no %s set", strings.ToLower(label))
+        }
 
-        if required && validation == nil {
+        if validation == nil {
             validation = validateNotEmpty
         }
 
-		p := promptui.Prompt{
-			Label:    fmt.Sprintf("%s%s", strings.ToUpper(label[0:1]), label[1:]),
-			Validate: validation,
-			Default:  dflt,
-		}
-		nme, err := p.Run()
-		if err != nil {
-			return "", err
-		}
-		value = nme
+        p := promptui.Prompt{
+            Label:    fmt.Sprintf("%s%s", strings.ToUpper(label[0:1]), label[1:]),
+            Validate: validation,
+            Default:  dflt,
+        }
+        nme, err := p.Run()
+        if err != nil {
+            return "", err
+        }
+        value = nme
 	}
 
 	return value, nil
