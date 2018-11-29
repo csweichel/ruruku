6d926b07a219d80e1f933d1146db2fb241515099
Christian Weichel <christian.weichel@typefox.io>
2018-11-29 06:00:02 +0000

    Adding REST support

--- build/protoc.sh
+++ build/protoc.sh
@@ -2,5 +2,10 @@
 
 wd=$(dirname $0)
 pushd $wd/.. &> /dev/null
-protoc -I pkg/server/api/ pkg/server/api/*.proto --go_out=plugins=grpc:pkg/server/api
+
+protoc -Ipkg/server/api/ \
+       -I../../grpc-ecosystem/grpc-gateway/third_party/googleapis \
+       pkg/server/api/v1/*.proto \
+       --go_out=plugins=grpc:pkg/server/api/v1
+
 popd  &> /dev/null
---
+++ build/protoc_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+go get -u -v github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u -v github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u -v github.com/golang/protobuf/protoc-gen-go
