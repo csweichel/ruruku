43f55fdd42b5ba8569b48fa95ec54f7d4d161812
Christian Weichel <christian.weichel@typefox.io>
2018-12-28 15:43:55 +0000

    Implemented version command
    Fixes 32leaves/ruruku#67

--- build/build_release.sh
+++ build/build_release.sh
@@ -14,4 +14,4 @@ go get -v github.com/inconshreveable/mousetrap
 go get -v github.com/konsorten/go-windows-terminal-sequences
 mkdir build/release
 rice embed-go -v -i github.com/32leaves/ruruku/pkg/server
-CGO_ENABLED=0 gox -os="$GOXOS" -arch="386 amd64" -output="build/release/ruruku$VERSION{{.OS}}_{{.Arch}}" -ldflags "-s -w -X cmd.Rev=`git rev-parse --short HEAD`" -verbose github.com/32leaves/ruruku
+CGO_ENABLED=0 gox -os="$GOXOS" -arch="386 amd64" -output="build/release/ruruku$VERSION{{.OS}}_{{.Arch}}" -ldflags "-s -w -X github.com/32leaves/ruruku/pkg/version.Tag=`git describe --abbrev=0` -X github.com/32leaves/ruruku/pkg/version.Rev=`git rev-parse --short HEAD` -X 'github.com/32leaves/ruruku/pkg/version.BuildDate=`date`'" -verbose github.com/32leaves/ruruku
