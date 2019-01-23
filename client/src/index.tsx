9c888911d86a8a2ba10ac1d9134b78a44c8bdaaf
Christian Weichel <christian.weichel@typefox.io>
2019-01-23 19:48:04 +0000

    Turned on autolinking for all markdown fields

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,11 @@ import App from './App';
 import { MiniEventEmitter } from './types/mini-event-emitter';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import * as showdown from 'showdown';
 
 const reloadRequest = new MiniEventEmitter<boolean>();
+
+showdown.setOption('simplifiedAutoLink', true);
 
 ReactDOM.render(
   <App reloadRequest={reloadRequest} />,
